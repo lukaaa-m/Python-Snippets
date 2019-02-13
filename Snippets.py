84d5994902db6f585a209ea47be3d22112be169f
lukaaa-m <43409006+lukaaa-m@users.noreply.github.com>
2019-02-13 14:43:59 +1000

    Fixed ask() function
    You're welcome Alex

--- Snippets.py
+++ Snippets.py
@@ -1,8 +1,8 @@
 def ask(question, expected_answers):
     validAnswer = False
-    answer = input(question)
+    answer = " " + (input(question)).lower() + " "
     for i in range(len(expected_answers)):
-        if expected_answers[i].lower() in answer.lower():
+        if (" " + expected_answers[i].lower() + " ") in answer:
                 validAnswer = True
     if not validAnswer:
             ask(question, expected_answers)
