7f84724ef830a08bd0b96833f0f0b60a9b7e1c00
lukaaa-m <lukablueowl51@gmail.com>
2019-02-17 22:55:47 +1000

    Added bad answer message
    If user enters a wrong answer, the programmer can now tell them that they're stupid before asking again

--- Snippets.py
+++ Snippets.py
@@ -1,9 +1,10 @@
 def ask(question, expected_answers):
     validAnswer = False
-    answer = " " + (input(question)).lower() + " "
+    answer = " " + (input(question[0])).lower() + " "
     for i in range(len(expected_answers)):
         if (" " + expected_answers[i].lower() + " ") in answer:
                 validAnswer = True
     if not validAnswer:
+            print(question[1])
             ask(question, expected_answers)
 
